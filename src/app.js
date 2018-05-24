60894dfe84b2be1b47e8ef510ad6ffd3fa4877a0
matthunterking <matthew85king@gmail.com>
2018-05-24 18:05:51 +0100

    Set up app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <h1>❤️ Destination Love ❤️</h1>
     );
   }
 }
