51cfc1a6cf18d55dee68d08bcc4b43a2def0e41e
Bridget <brturnbul@gmail.com>
2018-05-24 16:55:25 +0100

    login and register started

---
+++ controllers/auth.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/environment');
+
+function register(req, res, next) {
+  User.create(req.body)
+    .then(user => {
+
+      // creating the JWT and delivering the payload (sub user id)
+      // secret is in the end part of the token
+      // 'expiredIn' comes under the payload and is converted into seconds
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' });
+      res.json({
+        message: `Thanks for registering ${user.username}!`,
+        token,
+        user
+      });
+
+    })
+    .catch(next);
+}
+
+function login(req, res, next) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user || !user.validatePassword(req.body.password)) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' });
+      res.json({
+        message: `Welcome back ${user.username}!`,
+        token,
+        user
+      });
+    })
+    .catch(next);
+}
+
+module.exports = {
+  register,
+  login
+};
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+// const bodyParser = require('body-parser');
+
 const port = process.env.PORT || 4000;
+const router = require('./config/router');
+const { port, dbURI } = require('./config/environment');
 
 app.use(express.static(`${__dirname}/public`));
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbURI);
 
-
+app.use('/api', router);
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
