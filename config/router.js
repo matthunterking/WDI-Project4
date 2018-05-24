8ce56bb99c13c4f2bb35336d9c8aebdfd5778fb3
matthunterking <matthew85king@gmail.com>
2018-05-24 16:54:12 +0100

    Added router

--- config/router.js
+++ config/router.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const users = require('../controllers/users');
+const auth = require('../controllers/auth');
 
+router.route('/users')
+  .get(users.index);
+
+router.post('/register', auth.register);
+router.post('/login', auth.login);
 
 module.exports = router;
