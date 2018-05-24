b83828a7532cf4d78ad871573f3a75fb757a33f8
matthunterking <matthew85king@gmail.com>
2018-05-24 16:48:31 +0100

    Added environment file and set up basic router

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+
+const router = require('./config/router');
+const { port, dbURI } = require('./config/environment');
+
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbURI);
 
 app.use(express.static(`${__dirname}/public`));
-
+app.use('/api', router);
 
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
