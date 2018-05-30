7135beabcc9cced85ec7242d566047df27556017
Bridget <brturnbul@gmail.com>
2018-05-30 09:25:56 +0100

    midpoint between two dates

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 class Map extends React.Component {
 
-  constructor() {
-    super();
-    this.markers = [];
-  }
+  // constructor() {
+  //   super();
+  //   this.markers = [];
+  // }
 
   state = {
     users: [{
@@ -31,6 +31,11 @@ class Map extends React.Component {
       zoom: 14
     });
 
+    const centerMarker = new google.maps.InfoWindow({
+      content: '⭐️',
+      map: this.map
+    });
+
     this.marker1 = new google.maps.Marker({
       position: this.state.users[0].location,
       map: this.map,
@@ -42,8 +47,29 @@ class Map extends React.Component {
       label: '😍'
     });
 
-    this.generateMarkers();
+    const center = google.maps.geometry.spherical.interpolate(this.marker1.position, this.marker2.position, 0.5);
+    console.log(`CENTER --> lat: ${center.lat()} long: ${center.lng()}`);
+    centerMarker.setPosition(center);
+    this.map.setCenter(center);
+
+    var service = new google.maps.places.PlacesService(this.map);
+    service.nearbySearch({
+      location: centerMarker,
+      radius: 500,
+      type: ['bars']
+    });
   }
+
+  // function callback(results, status) {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //     for (var i = 0; i < results.length; i++) {
+  //       createMarker(results[i]);
+  //     }
+  //   }
+
+
+
+
 
   generateMarkers = () => {
     if(!this.props.markers) return false;
