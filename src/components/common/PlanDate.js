09c77839f2abe34b138f9c014fbffe269ee3eb4e
Bridget <brturnbul@gmail.com>
2018-05-30 15:45:16 +0100

    new snazzy map

--- src/components/common/PlanDate.js
+++ src/components/common/PlanDate.js
@@ -23,7 +23,197 @@ class PlanDate extends React.Component {
   componentDidMount() {
     this.map = new google.maps.Map(this.mapDiv, {
       center: { lat: 51.515, lng: -0.072 },
-      zoom: 14
+      zoom: 14,
+      styles: [
+        {
+          'featureType': 'road',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#FFFAF0'
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'stylers': [
+            {
+              'color': '#d9edf7'
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'elementType': 'labels',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'lightness': 40
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.park',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'on',
+              'color': '#c5dac6'
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape.natural.terrain',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#CCAA88'
+            },
+            {
+              'lightness': 40
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape.man_made',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#EEEEEE'
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'stylers': [
+            {
+              'visibility': 'simplified'
+            },
+            {
+              'color': '#FF0000'
+            },
+            {
+              'gamma': 9
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#FF0000'
+            },
+            {
+              'gamma': 8
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway.controlled_access',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#FF0000'
+            },
+            {
+              'gamma': 4
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'elementType': 'labels',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.government',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#DDDDDD'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.station',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#CCCCCC'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.line',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#AAAAAA'
+            },
+            {
+              'gamma': 4
+            }
+          ]
+        }
+      ]
     });
 
     const centerMarker = new google.maps.InfoWindow({
@@ -68,7 +258,7 @@ class PlanDate extends React.Component {
         return new google.maps.Marker({
           position: bar.location,
           map: this.map,
-          label: '🥂'
+          label: '💛'
         });
       });
     });
@@ -98,10 +288,10 @@ class PlanDate extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <div>
+        <h1 className="title is-1">Bars</h1>
         <div className="map" ref={ el => this.mapDiv = el }></div>
-        <div>
-          <h1 className="title is-1">Bars</h1>
+        <div className="container">
           {!this.state.bars && <p>Loading...</p>}
           <div className="columns is-multiline">
             {this.state.bars && this.state.bars.map(bar =>
@@ -111,9 +301,7 @@ class PlanDate extends React.Component {
                   style={{ backgroundImage: `url(${bar.image})` }}
                 ></div>
                 <div className="card-content">
-                  <div className="media">
-                    <p className="subtitle is-5">{bar.name}</p>
-                  </div>
+                  <p className="subtitle is-5">{bar.name}</p>
                 </div>
               </div>
             )}
